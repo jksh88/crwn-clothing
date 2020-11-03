ac3ac5af51a2178fa96ec641f7665d7ea2ff9d55
Joohoon Kim <joohoonkim@joohoons-mbp.attlocal.net>
2020-11-03 12:57:25 -0500

    updated menu-items to navigate with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPagge = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HatsPagge</h1>
+      <Link to={`${props.match.url}/13`}>To HAT 13</Link>
+      <Link to={`${props.match.url}/18`}>To HAT 18</Link>
+      <Link to={`${props.match.url}/20`}>To HAT 20</Link>
+    </div>
+  );
+};
+
+const HatsDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hat Product ID: {props.match.params.hatsId}</h1>
+    </div>
+  );
+};
+
+const MensPage = (props) => (
+  <div>
+    <h1>Mens Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/hats" component={HatsPagge}></Route>
+        <Route exact path="/hats/:hatsId" component={HatsDetail}></Route>
+        <Route exact path="/mens" render={MensPage} />
+        <Route exact path="/" render={HomePage} />
+      </Switch>
     </div>
   );
 }
